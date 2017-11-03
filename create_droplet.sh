4c416e381144d0848a963a8848617745fe8eefa1
Florian Kempenich <Shockn745@gmail.com>
2017-11-03 17:58:25 +0000

    Load droplet specs from file

--- create_droplet.sh
+++ create_droplet.sh
@@ -3,13 +3,19 @@
 if [ -z "$1" ]; then
   echo "Please provide a name for the Droplet"
   echo ""
-  echo "    \`create_droplet.sh DROPLET_NAME\`"
+  echo "    \`create_droplet.sh DROPLET_NAME (DROPLET_SPEC_NAME)\`"
   echo ""
   exit 1
 fi
 
 
 DROPLET_NAME=$1
+if [ -z "$2" ]; then
+  SPEC_NAME=micro
+else
+  SPEC_NAME=$2
+fi
+
 
 
 # `cd` in ansible directory to get ansible config: `ansible.cfg` and `inventory` directory
@@ -17,4 +23,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/ansible
 
 # Run "Create droplet" playbook
-ansible-playbook ./create-droplet-playbook.yml --extra-vars "droplet_name=$DROPLET_NAME"
+ansible-playbook \
+    ./create-droplet-playbook.yml \
+    --extra-vars "droplet_name=$DROPLET_NAME droplet_spec_name=$SPEC_NAME"
