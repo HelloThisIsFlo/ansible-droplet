745cc624daa15a3d81c3e3e8b794409c4a4a2ba4
Florian Kempenich <Shockn745@gmail.com>
2017-12-03 20:13:24 +0000

    Update setup.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+TBD
+
 ### Resources
 
 Glances as systemd service: https://github.com/nicolargo/glances/wiki/Start-Glances-through-Systemd
--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
-    name='ansible-droplet',
+    name='droplet',
     version='0.1',
-    scripts=['droplet.py']
+    description='A cli to Create / Destroy DigitalOcean Droplets',
+    long_description=readme(),
+    keywords='digitalocean digital ocean droplet ansible ssh provision',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+    ],
+    url='https://github.com/FlorianKempenich/ansible-droplet',
+    author='Florian Kempenich',
+    author_email='shockn745@gmail.com',
+    license='MIT',
+    scripts=['bin/droplet'],
+    install_requires=[
+        'ansible',
+        'dopy==0.3.5',
+        'click'
+    ],
+    include_package_data=True,
+    zip_safe=False
 )
+# TODO: Investigate the `zip_safe` option
